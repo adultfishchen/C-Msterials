ea217b1b1ec736319509a757e74082e1e1a772e0
adultfishchen <adultfish17640336@gmail.com>
2021-03-22 18:45:20 +0800

    Add TA Example

--- w4/ClassPractice/Program.cs
+++ w4/ClassPractice/Program.cs
@@ -14,27 +14,27 @@ namespace ClassPractice
     {
         static void function1()
         {
-            int  i,j,k,l,n;           
+            int  i,j,k,r,n;           
             Console.Write("Enter the Range = ");    
             n= int.Parse(Console.ReadLine());     
             for(i=1; i<=n; i++)      
             {          
-                //right side space
+                //數字三角形左側的空白數量為三角形高度(n)-目前第幾行(i)
                 for(j=1; j<=n-i; j++)      
                 {      
                 Console.Write(" ");      
                 }
 
-                //right side number
+                //數字三角形左半邊為等差數列由1至i
                 for(k=1;k<=i;k++)      
                 {      
                 Console.Write(k);      
                 }      
 
-                //left side number
-                for(l=i-1;l>=1;l--)      
+                //數字三角形右半邊為等差數列由i-1至1
+                for(r=i-1;r>=1;r--)      
                 {      
-                Console.Write(l);      
+                Console.Write(r);      
                 }      
                 Console.Write("\n");   
             }    
@@ -150,8 +150,27 @@ namespace ClassPractice
                 break;
             }
 
-            
-
+            //TA Example for randomly ID gereration
+            /*Random rnd = new Random();
+			//prefix
+			int p  = rnd.Next(0, 7);
+			Console.Write(prefix[p]);
+			
+			//first
+			int f  = rnd.Next(0, 6);
+			Console.Write(first[f]);
+			
+			//infix
+			int i  = rnd.Next(0, 8);
+			Console.Write(infix[i]);
+			
+			//second
+			int s  = rnd.Next(0, 6);
+			Console.Write(second[s]);
+			
+			//postfix
+			Console.Write(postfix[p]);
+			Console.Write("\n")*/;
         }
 
         static void function3()
@@ -182,6 +201,32 @@ namespace ClassPractice
                 num = Convert.ToInt32(Console.ReadLine());
             }
             Console.WriteLine("A sequence of reversed number: " + output);
+
+            //TA Example
+            /*
+			string sequence = "";
+			while(true){
+				Console.Write("Enter a number: ");
+				string number = Console.ReadLine();
+				
+				if(Convert.ToInt32(number)>=0){
+					Console.Write("Reversed number: ");
+					
+					//由string最後一個index至第一個index
+					for (int i=number.Length-1; i>=0; i--){
+						Console.Write(number[i]);
+						
+						//string的加法就是合併
+						sequence += number[i];
+					}
+					Console.Write("\n");
+				}
+				else{
+					Console.WriteLine("A sequence of reversed number: " + sequence);
+					break;
+				}
+			}
+            */
 
         }
 

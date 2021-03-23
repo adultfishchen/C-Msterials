6257dc71e2b2c9fca3b7ad8375be8bf0bf5f9563
adultfishchen <adultfish17640336@gmail.com>
2021-03-23 19:59:04 +0800

    Add W5 ClassPractice

---
+++ W5/ClassPractice/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+* function1 : Drink bar menu
+* function2 : Pascal's triangle
+* function3 : Spreading
+* 03/26/2021
+*/
+
+using System;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void function1()
+        {
+            Console.WriteLine("Enter base : ");
+            string Buserinput = Console.ReadLine();
+            string[] Base = Buserinput.Split(' ');
+            Console.WriteLine("Enter fruit : ");
+            string fuserinput = Console.ReadLine();
+            string[] fruit = fuserinput.Split(' ');
+            Console.WriteLine("Menu : ");
+            foreach (string str1 in Base)
+            {
+                foreach (string str2 in fruit)
+                {
+                    Console.Write(str2 + str1 + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void function2()
+        {
+            int rows, val = 1, blank, i, j;
+            Console.Write("Please input height of the Pascal's triangle : ");
+            rows = int.Parse(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine("Pascal's triangle");
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + " ");
+                    sum += val;
+                }
+                for (blank = 1; blank <= rows - i; blank++)
+                    Console.Write(" ");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Sum of all integers equals to " + sum);
+        }
+
+        static void function3()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            function1();
+            Console.WriteLine();
+            function2();
+            Console.WriteLine();
+            // function3();
+            // Console.WriteLine();
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
--- w4/ClassPractice/Program.cs
+++ w4/ClassPractice/Program.cs
@@ -6,7 +6,7 @@
 */
 
 using System;
-using System.Linq;  
+using System.Linq;
 
 namespace ClassPractice
 {
@@ -14,140 +14,140 @@ namespace ClassPractice
     {
         static void function1()
         {
-            int  i,j,k,r,n;           
-            Console.Write("Enter the Range = ");    
-            n= int.Parse(Console.ReadLine());     
-            for(i=1; i<=n; i++)      
-            {          
+            int i, j, k, r, n;
+            Console.Write("Enter the Range = ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
                 //數字三角形左側的空白數量為三角形高度(n)-目前第幾行(i)
-                for(j=1; j<=n-i; j++)      
-                {      
-                Console.Write(" ");      
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
                 }
 
                 //數字三角形左半邊為等差數列由1至i
-                for(k=1;k<=i;k++)      
-                {      
-                Console.Write(k);      
-                }      
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+                }
 
                 //數字三角形右半邊為等差數列由i-1至1
-                for(r=i-1;r>=1;r--)      
-                {      
-                Console.Write(r);      
-                }      
-                Console.Write("\n");   
-            }    
+                for (r = i - 1; r >= 1; r--)
+                {
+                    Console.Write(r);
+                }
+                Console.Write("\n");
+            }
         }
 
         static void function2()
         {
             string[] output = new string[5];
-            string[] pre = new string[] {"0o", "乂☆", "\\", "↗", "＊*", "卍"};
-            string[] post = new string[]{"0o", "☆乂", "/", "↙", "*＊", "卍"};
-            string[] infix = new string[]{"a", "o", "x", "之", "★", "～", "‧"};
-            string[] fir = new string[]{"小小", "元氣", "楓糖", "霸氣", "煞氣", "闇夜", "血"};
-            string[] sec = new string[]{"糖", "漾", "戀果", "鮭魚", "鮪魚", "大閘蟹"};
+            string[] pre = new string[] { "0o", "乂☆", "\\", "↗", "＊*", "卍" };
+            string[] post = new string[] { "0o", "☆乂", "/", "↙", "*＊", "卍" };
+            string[] infix = new string[] { "a", "o", "x", "之", "★", "～", "‧" };
+            string[] fir = new string[] { "小小", "元氣", "楓糖", "霸氣", "煞氣", "闇夜", "血" };
+            string[] sec = new string[] { "糖", "漾", "戀果", "鮭魚", "鮪魚", "大閘蟹" };
 
             //Determine what type it is
             Random rnd0 = new Random();
-            int N0  = rnd0.Next(0, 4);
+            int N0 = rnd0.Next(0, 4);
             //Console.WriteLine(N0);
-            
-            
-            switch(N0)
+
+
+            switch (N0)
             {
-                case 0 :
-                //For pre, post and sec
-                Random rnd1 = new Random();
-                int N1  = rnd1.Next(0, 5);
-               // Console.WriteLine(N1);
-                output[0]=pre[N1];
-                output[3]=sec[N1];
-                output[4]=post[N1];
+                case 0:
+                    //For pre, post and sec
+                    Random rnd1 = new Random();
+                    int N1 = rnd1.Next(0, 5);
+                    // Console.WriteLine(N1);
+                    output[0] = pre[N1];
+                    output[3] = sec[N1];
+                    output[4] = post[N1];
 
-                //For infix and first
-                Random rnd2 = new Random();
-                int N2  = rnd2.Next(0, 6);
-                //Console.WriteLine(N2);
-                output[1]=fir[N2];
-                output[2]=infix[N2];
+                    //For infix and first
+                    Random rnd2 = new Random();
+                    int N2 = rnd2.Next(0, 6);
+                    //Console.WriteLine(N2);
+                    output[1] = fir[N2];
+                    output[2] = infix[N2];
 
-                Console.WriteLine("Your Id is : " );
-                foreach(string str in output)
-                {
-                    Console.Write("{0}",str);
-                }
+                    Console.WriteLine("Your Id is : ");
+                    foreach (string str in output)
+                    {
+                        Console.Write("{0}", str);
+                    }
 
-                break;
+                    break;
 
-                case 1 :
-                //For pre, post and sec
-                Random rnd3 = new Random();
-                int N3  = rnd3.Next(0, 5);
-               //Console.WriteLine(N3);
-                output[0]=pre[N3];
-                output[2]=sec[N3];
-                output[3]=post[N3];
+                case 1:
+                    //For pre, post and sec
+                    Random rnd3 = new Random();
+                    int N3 = rnd3.Next(0, 5);
+                    //Console.WriteLine(N3);
+                    output[0] = pre[N3];
+                    output[2] = sec[N3];
+                    output[3] = post[N3];
 
-                //For first
-                Random rnd4 = new Random();
-                int N4  = rnd4.Next(0, 6);
-                //Console.WriteLine(N4);
-                output[1]=fir[N4];
-                
-                Console.WriteLine("Your Id is : " );
-                foreach(string str in output)
-                {
-                    Console.Write("{0}",str);
-                }
+                    //For first
+                    Random rnd4 = new Random();
+                    int N4 = rnd4.Next(0, 6);
+                    //Console.WriteLine(N4);
+                    output[1] = fir[N4];
 
-                break;
+                    Console.WriteLine("Your Id is : ");
+                    foreach (string str in output)
+                    {
+                        Console.Write("{0}", str);
+                    }
 
-                case 2 :
-                //For sec
-                Random rnd5 = new Random();
-                int N5  = rnd5.Next(0, 5);
-                //Console.WriteLine(N5);
-                output[2]=sec[N5];
+                    break;
 
-                //For infix and first
-                Random rnd6 = new Random();
-                int N6  = rnd6.Next(0, 6);
-                //Console.WriteLine(N6);
-                output[0]=fir[N6];
-                output[1]=infix[N6];
+                case 2:
+                    //For sec
+                    Random rnd5 = new Random();
+                    int N5 = rnd5.Next(0, 5);
+                    //Console.WriteLine(N5);
+                    output[2] = sec[N5];
 
-                Console.WriteLine("Your Id is : " );
-                foreach(string str in output)
-                {
-                    Console.Write("{0}",str);
-                }
+                    //For infix and first
+                    Random rnd6 = new Random();
+                    int N6 = rnd6.Next(0, 6);
+                    //Console.WriteLine(N6);
+                    output[0] = fir[N6];
+                    output[1] = infix[N6];
 
-                break;
+                    Console.WriteLine("Your Id is : ");
+                    foreach (string str in output)
+                    {
+                        Console.Write("{0}", str);
+                    }
 
-                case 3 :
-                //For sec
-                Random rnd7 = new Random();
-                int N7  = rnd7.Next(0, 5);
-                //Console.WriteLine(N7);
-                output[1]=pre[N7];
-                
+                    break;
 
-                //For and first
-                Random rnd8 = new Random();
-                int N8  = rnd8.Next(0, 6);
-                //Console.WriteLine(N8);
-                output[0]=fir[N8];
+                case 3:
+                    //For sec
+                    Random rnd7 = new Random();
+                    int N7 = rnd7.Next(0, 5);
+                    //Console.WriteLine(N7);
+                    output[1] = pre[N7];
 
-                Console.WriteLine("Your Id is : " );
 
-                foreach(string str in output)
-                {
-                    Console.Write("{0}",str);
-                }
+                    //For and first
+                    Random rnd8 = new Random();
+                    int N8 = rnd8.Next(0, 6);
+                    //Console.WriteLine(N8);
+                    output[0] = fir[N8];
 
-                break;
+                    Console.WriteLine("Your Id is : ");
+
+                    foreach (string str in output)
+                    {
+                        Console.Write("{0}", str);
+                    }
+
+                    break;
             }
 
             //TA Example for randomly ID gereration
@@ -175,26 +175,26 @@ namespace ClassPractice
 
         static void function3()
         {
-            int num,r,sum=0,t;
+            int num, r, sum = 0, t;
             string output = "";
-	
+
             Console.Write("\n\n");
             Console.Write("Display the number in reverse order:\n");
             Console.Write("--------------------------------------");
-            Console.Write("\n\n");  
+            Console.Write("\n\n");
 
             Console.Write("Input a number: ");
-            num = Convert.ToInt32(Console.ReadLine()); 	
+            num = Convert.ToInt32(Console.ReadLine());
 
             //依序取出個位、十位...的數值
-            while(num>0)
+            while (num > 0)
             {
-                for(t=num;t!=0;t=t/10)
+                for (t = num; t != 0; t = t / 10)
                 {
-                    r=t % 10;
-                    sum=sum*10+r;
+                    r = t % 10;
+                    sum = sum * 10 + r;
                 }
-                Console.Write("The number in reverse order is : {0} \n",sum);
+                Console.Write("The number in reverse order is : {0} \n", sum);
                 output += sum.ToString();
                 sum = 0;
                 Console.Write("Input a number: ");
